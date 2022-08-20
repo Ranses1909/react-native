ab0bfdac05f24982f988989dea5ee81a6db0f7c3
ranses1909 <canelonranses2@gmail.com>
2022-08-20 08:30:21 -0500

    main

--- 03-CalculadoraApp/src/components/reducer.tsx
+++ 03-CalculadoraApp/src/components/reducer.tsx
@@ -1,51 +1,74 @@
+import React from "react";
+
 const reducer = (state:any , action:any) => {
+    const {number, previousNumber, btnText} = state
     if(action.type === 'CLEAR'){
-    return {...state, number: '0'}
+    return {...state, number: '0', previousNumber: '0'}
     };
     
 
  if(action.type === 'CALCULATE'){ 
-          // IF YOU ARE PASSING JUST ONE PARAMETER YOU DON'T NEED TO PUT THE NAME AFTER .payload for example "action.payload.btnText"
-          
-if(state.number.includes('.') && action.payload === '.'){
-    // we grab the already store state in the variable number that's why we used .includes, an then we grab the btn we are clicking on with action.payload and we compare, if they are the same we change nothing
-  return {...state}
-}
-
-// If the first btn pressed is zero then we have to take into consideration...
-if(state.number.startsWith('0') || state.number.startsWith('-0')){
-
-    // if the btn pressed after zero it's '.' we leave the state how it is because we don't want to wipe out the zero before the dot
-    if(action.payload === '.'){
-       return{...state, number: state.number + action.payload} 
-    }
-    // if there's a dot in the state and zero is being pressed it is allowed 
-    else if( action.payload === '0' && state.number.includes('.')){
-        return{...state, number: state.number + action.payload} 
-    }
-    // now if we have zero in the state and zero btn is being pressed we don't take it into account okay? and also to change the default zero to the number being pressed if it is needed.
-    else{
-        return {
-            ...state, 
-            number: state.number.replace('0','') + action.payload,
-            } 
-    }
-}    
-    else{
-  return {...state, number: state.number + action.payload}
-}
-    };
-
-    if(action.type === 'PN'){
-        if(state.number.includes('-')){
-        return { ...state,
-            // TO NOT have the problem "NUMBER only refers to a type, but is being used as a value here" YOU have to acces the state and then acces the property dumb ass
-        number: state.number.replace('-', '')
-        }}else{
-            return { ...state, number: '-' + state.number}
+        // we grab the already store state in the variable number that's why we used .includes, an then we grab the btn we are clicking on with action.payload and we compare, if they are the same we change nothing
+        if (state.number.includes('.') && action.payload === '.') return{...state}; 
+        else{
+            return {
+                ...state,
+                // default state.number = '0'
+                /*-1) If the first btn pressed is zero then we leave the state how it is
+                -2) if there's a dot in the state and zero is being pressed it is allowed
+                -3) if payload = '.' we concatenate the state with payload to avoid removing the state content 
+                */ 
+              number: state.number !=='0' || action.payload === '.' ? state.number+action.payload : action.payload  
+            }
         }
     };
 
+    if(action.type === 'PN'){
+        // let stateLength = state.number.lentgh
+        // if((stateLength === 1 && state.number.includes('0')) && action.payload === '+/-')  return { ...state, number: state.number}
+        if(state.number.includes('-')){
+               return { ...state,
+                number: state.number.replace('-', '')
+        }
+        }else if(action.payload === '+/-'){
+            return { ...state, number: '-' + state.number}
+    }};
+
+if (action.type === 'DELETE'){
+    let stateLength = state.number.length
+    if(stateLength === 1 || (stateLength === 2 && state.number.includes('-'))){  return {...state, number: '0'}}
+    return { ...state, number: state.number.slice(0, stateLength-1) }
+} 
+
+if(action.type === 'PREVIOUS'){
+    if (number.endsWith('.')){  return { ...state, previousNumber: number.slice(0,-1), number:'0'}}
+    else{ return { ...state, number:'0', previousNumber: number}}
+}
+  // Por que enum????
+  const enum Operators{
+    // se definen numeros representados por un nombre...
+    sumar, restar, multiplicar, dividir,
+  }
+const calculate = () =>{
+    const lastReference = React.useRef<Operators>() 
+    const num1 = Number(number)
+    const num2 = Number(previousNumber)
+    switch(lastReference.current){
+    case Operators.sumar:
+        return {...state, number:`${(num1 + num2)}`, previousNumber:'0'}
     }
+}
+
+if(action.type === 'DIVISION'){
+    if (action.payload === '/'){
+        const num1 = Number(number)
+        const num2 = Number(previousNumber)
+        return {...state, number:`${(num1 / num2)}`, previousNumber:number}
+    }
+    //  return { ...state, number: '0'} 
+}
+}   
 
 export default reducer
+    
+ 
--- 03-CalculadoraApp/src/screens/CalculadoraScreen.tsx
+++ 03-CalculadoraApp/src/screens/CalculadoraScreen.tsx
@@ -6,65 +6,108 @@ import reducer from '../components/reducer'
 
   const initialState = {
     number: '0',  //actual state.number
-    numBeforeNum: '0', // this is just for storing the number pressed before
-    previousNumber: "100", // this is  the previous result
+    previousNumber: "0", // this is  the previous result
     btnText: '0' // what is coming from btnCalc 
   }
-
+  // Por que enum????
+  // to make useRef look better basically so you avoid passing several properties in useref
+  const enum Operators{
+    // se definen numeros representados por un nombre...
+    sumar, restar, multiplicar, dividir,
+  }
+   
 const CalculadoraScreen = () => {
   const [state, dispatch] = useReducer(reducer, initialState);
-  
+  const lastReference = React.useRef<Operators>() 
+
+
   const limpiar = () => {
+    numBefore();
     dispatch({type: 'CLEAR'})
   }
-// calculate grabs the content of the btn
-const calculate = (btnText: string) => {
+// buidNumber grabs the content of the btn
+const buildNumber = (btnText: string) => {
 // we concatenate with number what we have in that specific btn
-dispatch({type: 'CALCULATE', payload: btnText})
+dispatch({type: 'BUILDNUMBER', payload: btnText})
+}
+const positiveNegative = (btnText: string) => {
+  dispatch({type: 'PN', payload: btnText})
+}
+const removeLast = () => {
+  dispatch({type: 'DELETE'})
+}
+const numBefore =() =>{
+dispatch({type: 'PREVIOUS'})
 }
 
-const positiveNegative = () => {
-  dispatch({type: 'PN'})
+
+
+const calcDiv = () => {
+  numBefore();
+  lastReference.current= Operators.dividir
+  dispatch({type: 'DIVISION'})
 }
+const calcMulti = () => {
+  numBefore();
+  lastReference.current= Operators.multiplicar
+  dispatch({type: 'MULTIPLICACION' })
+}
+const calcSum = () => {
+  numBefore();
+  lastReference.current= Operators.sumar
+  dispatch({type: 'SUMA' })
+}
+const calcResta = () => {
+  numBefore();
+  lastReference.current= Operators.restar
+  dispatch({type: 'RESTA' })
+}
+
+const calculate = () => {
+  dispatch({type: 'CALCULATE',payload:{lastReference, }})
+  }
+  
+
 
   return (
     <View style={styles.container}>
+      {(state.previousNumber !== '0') && 
         <Text style={styles.previousResult}>{state.previousNumber}</Text>
-        <Text 
+         }<Text
         style={styles.results}
           numberOfLines={1}
            adjustsFontSizeToFit={true}
            >
-            {state.number}</Text>
+            {state.number}</Text>  
 
         <View style={styles.row}>
           <BtnCalc content="AC" color="#9B9B9B" onclick={limpiar}/>
           <BtnCalc content="+/-" color={'#9B9B9B'} onclick={positiveNegative}/>
-          <BtnCalc content="del" color={'#9B9B9B'} onclick={limpiar} />
-          <BtnCalc content="/" color={'#FF9427'} onclick={limpiar}/>
+          <BtnCalc content="del" color={'#9B9B9B'} onclick={removeLast} />
+          <BtnCalc content="/" color={'#FF9427'} onclick={calcDiv}/>
           </View>
          <View  style={styles.row}>
-          <BtnCalc content="7" onclick={calculate} />
-          <BtnCalc content="8" onclick={calculate}/>
-          <BtnCalc content="9" onclick={calculate}/>
-          <BtnCalc content="x" color={'#FF9427'} onclick={calculate}/>
+          <BtnCalc content="7" onclick={buildNumber} />
+          <BtnCalc content="8" onclick={buildNumber}/>
+          <BtnCalc content="9" onclick={buildNumber}/>
+          <BtnCalc content="x" color={'#FF9427'} onclick={calcMulti}/>
           </View>
        <View style={styles.row}>
-          <BtnCalc content="4" onclick={calculate}/>
-          <BtnCalc content="5"onclick={calculate} />
-          <BtnCalc content="6" onclick={calculate}/>
-          <BtnCalc content="-" color={'#FF9427'} onclick={calculate}/>
+          <BtnCalc content="4" onclick={buildNumber}/>
+          <BtnCalc content="5"onclick={buildNumber} />
+          <BtnCalc content="6" onclick={buildNumber}/>
+          <BtnCalc content="-" color={'#FF9427'} onclick={calcResta}/>
           </View>
         <View style={styles.row}>
-          <BtnCalc content="1" onclick={calculate}/>
-          <BtnCalc content="2" onclick={calculate}/>
-          <BtnCalc content="3" onclick={calculate}/>
-          <BtnCalc content="+" color={'#FF9427'} onclick={calculate}/>
+          <BtnCalc content="1" onclick={buildNumber}/>
+          <BtnCalc content="2" onclick={buildNumber}/>
+          <BtnCalc content="3" onclick={buildNumber}/>
+          <BtnCalc content="+" color={'#FF9427'} onclick={calcSum}/>
           </View>
         <View style={styles.row}>
-          <BtnCalc content="0" ancho={true} onclick={calculate} />
-          <BtnCalc content="." onclick={calculate}/>
-          <BtnCalc content="=" color={'#FF9427'} onclick={limpiar}/>
+          <BtnCalc content="0" ancho={true} onclick={buildNumber} />
+          <BtnCalc content="." onclick={buildNumber}/>
+          <BtnCalc content="=" color={'#FF9427'} onclick={numBefore}/>
           </View>
   </View>
   )
